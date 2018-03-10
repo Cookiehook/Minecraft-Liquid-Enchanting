7e063cfcf2470ff03e94ed5b6bd13f02fcde33d8
Cookiehook <shaun.harrison@live.co.uk>
2018-03-10 07:52:45 +0000

    Fixed issue where leather item sprites were always white

--- src/main/java/com/cookiehook/liquidenchanting/init/ModItems.java
+++ src/main/java/com/cookiehook/liquidenchanting/init/ModItems.java
@@ -7,7 +7,10 @@ import com.cookiehook.liquidenchanting.util.Reference;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.renderer.color.IItemColor;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ModItems {
@@ -15,7 +18,8 @@ public class ModItems {
 	// constructors. Used for registration later.
 	public static List<Item> itemList = new ArrayList<Item>();
 
-	// Basic item to be used for creative tabs, game demos etc. Only available through command line.
+	// Basic item to be used for creative tabs, game demos etc. Only available
+	// through command line.
 	public static Item liquid_enchanting = new Item().setUnlocalizedName("liquid_enchanting").setRegistryName("liquid_enchanting");
 
 	// Calls the init() method of each class in this package, forcing the Item
@@ -53,11 +57,26 @@ public class ModItems {
 		// Loops through every Item in the itemList and renders it.
 		for (Item item : itemList) {
 			registerRender(item);
+			registerColor(item);
 		}
 	}
 
 	private static void registerRender(Item item) {
 		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(item, 0,
 				new ModelResourceLocation(Reference.MOD_ID + ":" + item.getUnlocalizedName().substring(5), "inventory"));
+	}
+
+	// Registers the overlay color for leather armor on the item sprites.
+	// If omitted, the items always show a white color, regardless of dyes.
+	private static void registerColor(Item item) {
+		if (item instanceof ItemArmor) {
+			Minecraft.getMinecraft().getItemColors().registerItemColorHandler(new IItemColor() {
+
+				@Override
+				public int getColorFromItemstack(ItemStack stack, int tintIndex) {
+					return tintIndex > 0 ? -1 : (((ItemArmor) stack.getItem()).getColor(stack));
+				}
+			}, item);
+		}
 	}
 }
