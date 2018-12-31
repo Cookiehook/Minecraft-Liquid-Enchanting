72a75c9f1a921cabbe5b5620f2f5986bf9a4dc18
Cookiehook <shaun.harrison@live.co.uk>
2018-12-31 12:47:07 +0000

    Fixed condition factory

--- src/main/java/com/cookiehook/liquidenchanting/util/crafting/BeaconConditionFactory.java
+++ src/main/java/com/cookiehook/liquidenchanting/util/crafting/BeaconConditionFactory.java
@@ -15,17 +15,18 @@ public class BeaconConditionFactory implements IConditionFactory {
     public BooleanSupplier parse(JsonContext context, JsonObject json) {
 
         String key = JsonUtils.getString(json, "type");
+        boolean value = JsonUtils.getBoolean(json , "value", true);
 
         switch (key) {
-        case Reference.MOD_ID + ":haste_armor_enabled": return () -> Config.configMap.get("haste_armor");
-        case Reference.MOD_ID + ":strong_haste_armor_enabled": return () -> Config.configMap.get("strong_haste_armor");
-        case Reference.MOD_ID + ":resistance_armor_enabled": return () -> Config.configMap.get("resistance_armor");
-        case Reference.MOD_ID + ":strong_resistance_armor_enabled": return () -> Config.configMap.get("strong_resistance_armor");
-        case Reference.MOD_ID + ":haste_sword_enabled": return () -> Config.configMap.get("haste_sword");
-        case Reference.MOD_ID + ":strong_haste_sword_enabled": return () -> Config.configMap.get("strong_haste_sword");
-        case Reference.MOD_ID + ":resistance_sword_enabled": return () -> Config.configMap.get("resistance_sword");
-        case Reference.MOD_ID + ":strong_resistance_sword_enabled": return () -> Config.configMap.get("strong_resistance_sword");
-            default: return () -> false;
+        case Reference.MOD_ID + ":haste_armor_enabled": return () -> Config.configMap.get("haste_armor") == value;
+        case Reference.MOD_ID + ":strong_haste_armor_enabled": return () -> Config.configMap.get("strong_haste_armor") == value;
+        case Reference.MOD_ID + ":resistance_armor_enabled": return () -> Config.configMap.get("resistance_armor") == value;
+        case Reference.MOD_ID + ":strong_resistance_armor_enabled": return () -> Config.configMap.get("strong_resistance_armor") == value;
+        case Reference.MOD_ID + ":haste_sword_enabled": return () -> Config.configMap.get("haste_sword") == value;
+        case Reference.MOD_ID + ":strong_haste_sword_enabled": return () -> Config.configMap.get("strong_haste_sword") == value;
+        case Reference.MOD_ID + ":resistance_sword_enabled": return () -> Config.configMap.get("resistance_sword") == value;
+        case Reference.MOD_ID + ":strong_resistance_sword_enabled": return () -> Config.configMap.get("strong_resistance_sword") == value;
+            default: return () -> !value;
         }
     }
 }
